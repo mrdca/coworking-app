071a5770a9cf339db3d422bc531039e7dbd44fad
David Collazo <dcalvarez1994@gmail.com>
2024-03-15 20:23:08 -0600

    Modify config

--- analytics/config.py
+++ analytics/config.py
@@ -4,11 +4,11 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = "myuser"
+db_username = "postgres"
 db_password = "mypassword"
 db_host = "postgresql-service"
 db_port = "5433"
-db_name = "mydatabase"
+db_name = "postgres"
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
